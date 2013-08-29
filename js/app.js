071cba1c31565906f9d0bbd7dbc1c4527a7f0b39
Kristjan Jansen <kristjan.jansen@gmail.com>
2013-08-29 23:47:50 +0300

    phew

--- js/app.js
+++ js/app.js
@@ -1,7 +1,52 @@
-$.get('./slides/index.txt', function(data) {
+//$(document).ready(function() {
+/*
+  page('/', renderIndex)
+*/  
+/*
+  page({dispatch: true, click: false})
 
-  $('body').html(
-    Mustache.render($('#index').html(), {index: data.split('\n')})
-  )
+  $('a').on('click', function(ev) {
+    var dir = $(this).attr(href)
+    console.log(dir)
+  })
+*/
   
-}, 'text')
+//})
+
+$(document).ready(function() {
+
+page.base('/tmp/ak_slides2')
+page('/slides/:slides/page', renderSlides)
+page('/', renderIndex)
+page({dispatch: true})
+
+})
+
+
+function renderIndex(ctx, next) {
+  
+  $.get('./slides/index.txt', function(data) {
+
+    $('.wrapper').html(
+      Mustache.render($('#index').html(), {index: data.split('\n')})
+    )
+    next()
+  
+  }, 'text')
+
+}
+
+function renderSlides(ctx, next) {
+  
+  console.log(Mustache.render($('#slides').html(), {slide: ctx.params.slides}))
+  
+  $.get('./slides/' + ctx.params.slides, function(data) {
+    var slides = data.split('\n\n\n')
+
+    $('.wrapper').html(
+      Mustache.render($('#slides').html(), {slide: ctx.params.slides})    
+    )
+
+  }, 'text')
+
+}
