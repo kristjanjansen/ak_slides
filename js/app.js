034e5aa9cafe65429b3df27c973668ad99f7bef8
Kristjan Jansen <kristjan.jansen@gmail.com>
2013-08-31 22:34:55 +0300

    moving on

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 page('/', renderIndex)
 page('/:slide', renderSlides)
+page('/:slide/slideshow', renderSlideshow)
 page({dispatch: true, click: true})
 
 function renderIndex(ctx, next) {
@@ -16,7 +17,9 @@ function renderIndex(ctx, next) {
 
 function renderSlides(ctx, next) {
     
-  $.get('./slides/' + ctx.params.slide, function(data) {
+  $.get('/slides/' + ctx.params.slide, function(data) {
+    
+    var y = 0;
     
     var slides = data
       .split('\n\n\n')
@@ -26,11 +29,46 @@ function renderSlides(ctx, next) {
       .map(function(item) {
         return item.replace(/href/g, 'target="_blank" href')
       })
-        
+      .map(function(item) {
+        return {slide: item, y: y}
+        y = y + 1000
+      })
+              
     $('.wrapper').html(
       Mustache.render($('#slides').html(), {slide: ctx.params.slide, slides: slides})    
     )
 
+  }, 'text')
+
+}
+
+function renderSlideshow(ctx, next) {
+    
+  $.get('/slides/' + ctx.params.slide, function(data) {
+    
+    var slides = []
+    var x = 0
+    
+    var data = data
+      .split('\n\n\n')
+      .map(function(item) { 
+        return marked(item, {breaks: true})
+      })
+      .map(function(item) {
+        return item.replace(/href/g, 'target="_blank" href')
+      })
+      .forEach(function(item) {
+        slides.push({slide: item, x: x += 1000})
+      })
+  
+    // console.log(slides)
+    
+    $('.wrapper').html(
+      Mustache.render($('#slideshow').html(), {slide: ctx.params.slide, slides: slides})    
+    )
+
+    impress().init()
+    
   }, 'text')
 
 }
